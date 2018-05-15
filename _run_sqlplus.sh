0abc37823ed04abc8281d2f9c6f219599b5c2723
hayden <hhudson@uchicago.edu>
2018-05-15 16:54:03 -0400

    latest

---
+++ _run_sqlplus.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# make sure script uses correct environment settings for sqlplus
+source ~/.profile
+
+# run sqlplus, execute the script, then get the error list and exit
+sql $1 << EOF
+$2
+@_show_errors.sql
+exit;
+EOF
---
+++ trunk/release/_release.sql
@@ -0,0 +1,35 @@
+@trunk/build/create_tables.sql;
+/*@trunk/build/create_indexes.sql;
+@trunk/sequences/blog_seq.sql;
+@trunk/build/create_synonyms.sql;
+@trunk/build/functions.sql;
+@trunk/build/create_views.sql;
+@trunk/packages/blog_log_pkg.pks;
+@trunk/packages/blog_log_pkg.pkb;
+@trunk/packages/blog_xml_pkg.pks;
+@trunk/packages/blog_xml_pkg.pkb;
+@trunk/packages/blog_util_pkg.pks;
+@trunk/packages/blog_util_pkg.pkb;
+@trunk/packages/blog_plugin_pkg.pks;
+@trunk/packages/blog_plugin_pkg.pkb;
+@trunk/packages/blog_job_pkg.pks;
+@trunk/packages/blog_job_pkg.pkb;
+@trunk/packages/blog_install_pkg.pks;
+@trunk/packages/blog_install_pkg.pkb;
+@trunk/packages/blog_admin_app_pkg.pks;
+@trunk/packages/blog_admin_app_pkg.pkb;
+@trunk/build/triggers.sql;
+@trunk/seed/parameter_data.sql;
+@trunk/seed/country_data.sql;
+@trunk/seed/admin.sql;
+@trunk/seed/long.sql;
+@trunk/build/fk.sql;
+@trunk/seed/theme_files.sql;
+/
+BEGIN
+  blog_install.set_jobs;
+  blog_install.update_param_data;
+END;
+/
+@trunk/apex/209021.sql;
+@trunk/apex/f427.sql;*/
