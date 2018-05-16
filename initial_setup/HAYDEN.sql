4dca0889e87089063ee236705927a7169892c7ba
hayden <hhudson@uchicago.edu>
2018-05-16 16:20:34 -0400

    correcting scripts to work with the Hayden schema

--- initial_setup/HAYDEN.sql
+++ initial_setup/HAYDEN.sql
@@ -13,18 +13,18 @@ whenever sqlerror exit sql.sqlcode rollback
 begin
 wwv_flow_api.import_begin (
  p_version_yyyy_mm_dd=>'2016.08.24'
-,p_default_workspace_id=>3792733197808802
+,p_default_workspace_id=>5510843774547541
 );
 end;
 /
-prompt  WORKSPACE 3792733197808802
+prompt  WORKSPACE 5510843774547541
 --
 -- Workspace, User Group, User, and Team Development Export:
---   Date and Time:   12:42 Friday May 11, 2018
+--   Date and Time:   15:49 Sunday May 6, 2018
 --   Exported By:     HAYDEN
 --   Export Type:     Workspace Export
---   Version:         5.1.1.00.08
---   Instance ID:     218201873459159
+--   Version:         5.1.3.00.05
+--   Instance ID:     108840466845548
 --
 -- Import:
 --   Using Instance Administration / Manage Workspaces
@@ -32,7 +32,7 @@ prompt  WORKSPACE 3792733197808802
 --   Using SQL*Plus as the Oracle user APEX_050100
  
 begin
-    wwv_flow_api.set_security_group_id(p_security_group_id=>3792733197808802);
+    wwv_flow_api.set_security_group_id(p_security_group_id=>5510843774547541);
 end;
 /
 ----------------
@@ -42,12 +42,13 @@ end;
 prompt  Creating workspace HAYDEN...
 begin
 wwv_flow_fnd_user_api.create_company (
-  p_id => 3792939793808905
- ,p_provisioning_company_id => 3792733197808802
+  p_id => 5511057652547639
+ ,p_provisioning_company_id => 5510843774547541
  ,p_short_name => 'HAYDEN'
  ,p_display_name => 'HAYDEN'
  ,p_first_schema_provisioned => 'HAYDEN'
- ,p_company_schemas => 'C2REST:HAYDEN'
+ ,p_company_schemas => 'HAYDEN'
+ ,p_ws_schema => 'HAYDEN'
  ,p_account_status => 'ASSIGNED'
  ,p_allow_plsql_editing => 'Y'
  ,p_allow_app_building_yn => 'Y'
@@ -59,7 +60,7 @@ wwv_flow_fnd_user_api.create_company (
  ,p_allow_restful_services_yn => 'Y'
  ,p_source_identifier => 'HAYDEN'
  ,p_path_prefix => 'HAYDEN'
- ,p_files_version => 1
+ ,p_files_version => 2
 );
 end;
 /
@@ -99,19 +100,44 @@ prompt  Creating group grants...
 prompt  Creating Users...
 begin
 wwv_flow_fnd_user_api.create_fnd_user (
-  p_user_id                      => '12119608178071442',
-  p_user_name                    => 'ANTON',
-  p_first_name                   => '',
-  p_last_name                    => '',
+  p_user_id                      => '5510718096547541',
+  p_user_name                    => 'ADMIN',
+  p_first_name                   => 'Hayden',
+  p_last_name                    => 'Hudson',
   p_description                  => '',
-  p_email_address                => 'anton@concept2completion.com',
-  p_web_password                 => 'C934F2980842E0B43AE9D2098FCAE6227A19A4F69548EB7F326BC4909704A7024BAE4AF63EA2915152C69D959FEF85BCFE8EC0726B5796A0AE42535E528BDFF5',
+  p_email_address                => 'hayden@concept2completion.com',
+  p_web_password                 => '185FAEBFBFAFA9D10B4995F05DD1F56237A459ED219C057FDEE9424DADABE243CA22086B9CCC864044D747B0E3C227635E03DAC128DD2CF1495EA67123C53D38',
   p_web_password_format          => '5;5;10000',
-  p_group_ids                    => '',
+  p_group_ids                    => '1670163835171662:1670236590171665:1670338847171665:',
   p_developer_privs              => 'ADMIN:CREATE:DATA_LOADER:EDIT:HELP:MONITOR:SQL',
   p_default_schema               => 'HAYDEN',
   p_account_locked               => 'N',
-  p_account_expiry               => to_date('201710030000','YYYYMMDDHH24MI'),
+  p_account_expiry               => to_date('201607121444','YYYYMMDDHH24MI'),
+  p_failed_access_attempts       => 1,
+  p_change_password_on_first_use => 'Y',
+  p_first_password_use_occurred  => 'Y',
+  p_allow_app_building_yn        => 'Y',
+  p_allow_sql_workshop_yn        => 'Y',
+  p_allow_websheet_dev_yn        => 'Y',
+  p_allow_team_development_yn    => 'Y',
+  p_allow_access_to_schemas      => '');
+end;
+/
+begin
+wwv_flow_fnd_user_api.create_fnd_user (
+  p_user_id                      => '12343960955403223',
+  p_user_name                    => 'BRAD.KNIGHT',
+  p_first_name                   => '',
+  p_last_name                    => '',
+  p_description                  => '',
+  p_email_address                => 'brad.knight@oracle.com',
+  p_web_password                 => 'D0984A1AFD487FC1D536B37FBC6ACC2600A433F8F510DCEF8B2B781E67AE233073854DB0EA8AFEE261314C3AD18B7FE2F41B101B08F1039A4443289560588073',
+  p_web_password_format          => '5;5;10000',
+  p_group_ids                    => '',
+  p_developer_privs              => 'CREATE:EDIT:HELP:MONITOR:SQL:MONITOR:DATA_LOADER',
+  p_default_schema               => 'HAYDEN',
+  p_account_locked               => 'N',
+  p_account_expiry               => to_date('201702150000','YYYYMMDDHH24MI'),
   p_failed_access_attempts       => 0,
   p_change_password_on_first_use => 'N',
   p_first_password_use_occurred  => 'N',
@@ -124,22 +150,72 @@ end;
 /
 begin
 wwv_flow_fnd_user_api.create_fnd_user (
-  p_user_id                      => '3792646344808802',
+  p_user_id                      => '27138488369212325',
   p_user_name                    => 'HAYDEN',
   p_first_name                   => 'Hayden',
   p_last_name                    => 'Hudson',
   p_description                  => '',
   p_email_address                => 'hayden.hudson@concept2completion.com',
-  p_web_password                 => '41635A22EAF08A13134AC475D330A2F7FAE709BD79AB9DF14E46DFEE77238E4A4882525FF1DA8BFF07720EFD044D65A40A3D21E4196AA2795915715E5B60C0D0',
+  p_web_password                 => '32BF9FCAE342C8D75119E83C95ECC9C8141F844FB0E8CB399F40AD5939C26ECA14A3913128D12EA12DD4C7B6445D704CBDA635DD07EBD215508C2672D9BD8C8F',
+  p_web_password_format          => '5;5;10000',
+  p_group_ids                    => '1670163835171662:1670236590171665:1670338847171665:',
+  p_developer_privs              => 'ADMIN:CREATE:DATA_LOADER:EDIT:HELP:MONITOR:SQL',
+  p_default_schema               => 'HAYDEN',
+  p_account_locked               => 'N',
+  p_account_expiry               => to_date('201609190000','YYYYMMDDHH24MI'),
+  p_failed_access_attempts       => 0,
+  p_change_password_on_first_use => 'N',
+  p_first_password_use_occurred  => 'N',
+  p_allow_app_building_yn        => 'Y',
+  p_allow_sql_workshop_yn        => 'Y',
+  p_allow_websheet_dev_yn        => 'Y',
+  p_allow_team_development_yn    => 'Y',
+  p_allow_access_to_schemas      => '');
+end;
+/
+begin
+wwv_flow_fnd_user_api.create_fnd_user (
+  p_user_id                      => '12344941270414425',
+  p_user_name                    => 'JOHN.BELL',
+  p_first_name                   => '',
+  p_last_name                    => '',
+  p_description                  => '',
+  p_email_address                => 'john.bell@oracle.com',
+  p_web_password                 => '7D816822AE0B1E262EBBD5D8149524DC84E456446CA81720722BD1A02DBCF80811D74D5FFDF569F49FA6161A927AE0805E44E8FC6A12E89A8769623C15AEDE3F',
+  p_web_password_format          => '5;5;10000',
+  p_group_ids                    => '',
+  p_developer_privs              => '',
+  p_default_schema               => 'HAYDEN',
+  p_account_locked               => 'N',
+  p_account_expiry               => to_date('201702150000','YYYYMMDDHH24MI'),
+  p_failed_access_attempts       => 0,
+  p_change_password_on_first_use => 'N',
+  p_first_password_use_occurred  => 'N',
+  p_allow_app_building_yn        => 'Y',
+  p_allow_sql_workshop_yn        => 'Y',
+  p_allow_websheet_dev_yn        => 'Y',
+  p_allow_team_development_yn    => 'Y',
+  p_allow_access_to_schemas      => '');
+end;
+/
+begin
+wwv_flow_fnd_user_api.create_fnd_user (
+  p_user_id                      => '12344421345410396',
+  p_user_name                    => 'SUSIE.PARKER',
+  p_first_name                   => '',
+  p_last_name                    => '',
+  p_description                  => '',
+  p_email_address                => 'susie.parker@oracle.com',
+  p_web_password                 => '7869899788B11BD0AD3F7CEAD756F2D5FA45F291C4836510B462173D5ACE066CCA7AA517376B676C59DE64BC3F364A3CAE49523958ACCD1F719C616A5B10B48B',
   p_web_password_format          => '5;5;10000',
   p_group_ids                    => '',
   p_developer_privs              => 'ADMIN:CREATE:DATA_LOADER:EDIT:HELP:MONITOR:SQL',
   p_default_schema               => 'HAYDEN',
   p_account_locked               => 'N',
-  p_account_expiry               => to_date('201804250950','YYYYMMDDHH24MI'),
+  p_account_expiry               => to_date('201702150000','YYYYMMDDHH24MI'),
   p_failed_access_attempts       => 0,
-  p_change_password_on_first_use => 'Y',
-  p_first_password_use_occurred  => 'Y',
+  p_change_password_on_first_use => 'N',
+  p_first_password_use_occurred  => 'N',
   p_allow_app_building_yn        => 'Y',
   p_allow_sql_workshop_yn        => 'Y',
   p_allow_websheet_dev_yn        => 'Y',
--- initial_setup/sys.grants.sql
+++ initial_setup/sys.grants.sql
@@ -1,5 +1,6 @@
 alter session set "_ORACLE_SCRIPT"=true;  
-create user BLOG_2122 identified by Oradoc_db1;
+--irrelevant if I'm using the Hayden workspace and schema
+/*create user BLOG_2122 identified by Oradoc_db1;
 grant CREATE SESSION to BLOG_2122;
 grant CREATE MATERIALIZED VIEW to BLOG_2122;
 grant CREATE INDEXTYPE to BLOG_2122;
@@ -10,4 +11,4 @@ grant CREATE TABLE to BLOG_2122;
 grant CREATE TRIGGER to BLOG_2122;
 grant CREATE VIEW to BLOG_2122;
 grant CREATE JOB to BLOG_2122;
-alter user BLOG_2122 quota 25m on system;
+alter user BLOG_2122 quota 25m on system;*/
